3e940e61ac01726a9a69e8fa8b1963925e6e9b4b
Michael Ta <technical.ta@gmail.com>
2016-07-14 09:01:43 -0400

    testing endpoint on server

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,14 +1,60 @@
 var router = require('express').Router();
 var four0four = require('./utils/404')();
 var data = require('./data');
+var elasticsearch = require('elasticsearch');
 
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'error'
+});
+var index = 'prepared_responses';
+
+router.get('/search', doSearch);
+router.get('/questions', getQuestions);
 router.get('/people', getPeople);
-router.get('/person/:id', getPerson);
+// router.get('/person/:id', getPerson);
 router.get('/*', four0four.notFoundMiddleware);
 
 module.exports = router;
 
 //////////////
+
+
+function doSearch(req, res, next) {
+  client.search({
+    index: index,
+    body: {
+      query: {
+        query_string: {
+          query: 'ebola'
+        }
+      },
+      size: 1000,
+      explain: false     //set to 'true' for testing only
+    }
+  })
+  .then(function(results) {
+    var hits = results.hits.hits;
+    res.send(hits);
+  }, function(err) {
+    console.trace(err.message);
+  });
+}
+
+function getQuestions(req, res, next) {
+  client.search({
+    index: index,
+    body: {
+
+    }
+  })
+  .then(function(results) {
+    var hits = results.hits.hits;
+    res.send(hits);
+  }, function(err) {
+    console.trace(err.message);
+  });
+}
 
 function getPeople(req, res, next) {
   res.status(200).send(data.people);
