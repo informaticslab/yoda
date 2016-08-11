eff331688f442b2d6a7083f51736424bd1c9ae5a
Michael Ta <technical.ta@gmail.com>
2016-08-11 16:05:23 -0400

    scroll to top on paging

--- src/client/app/results/results.controller.js
+++ src/client/app/results/results.controller.js
@@ -5,9 +5,9 @@
     .module('app.results')
     .controller('ResultsController', ResultsController);
 
-  ResultsController.$inject = ['logger', '$scope', '$state', '$stateParams', 'dataservice', '$q'];
+  ResultsController.$inject = ['logger', '$scope', '$state', '$stateParams', 'dataservice', '$q', '$location', '$anchorScroll'];
   /* @ngInject */
-  function ResultsController(logger, $scope, $state, $stateParams, dataservice, $q) {
+  function ResultsController(logger, $scope, $state, $stateParams, dataservice, $q, $location, $anchorScroll) {
     var vm = this;
     vm.title = 'Results';
     vm.searchString = $stateParams.searchString;
@@ -16,6 +16,13 @@
     vm.currentPage = 1;
     vm.pageSize = 5;
     vm.noResults = false;
+
+    vm.toTop = function(id) {
+      var old = $location.hash();
+      $location.hash(id);
+      $anchorScroll();
+      $location.hash(old);
+    };
 
     vm.goToResults = function($item) {
       $state.go('details', {id: $item.id});
