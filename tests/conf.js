33fd3fd522dbccda9d06900d49a69dc92f34ea16
missilen <tnmissilen@gmail.com>
2016-10-11 17:22:46 -0400

    added id to search button for use with automated test;  added simple UI test cases

---
+++ tests/conf.js
@@ -0,0 +1,9 @@
+/**
+ * Created by trungnguyen on 10/11/16.
+ */
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js']
+}
