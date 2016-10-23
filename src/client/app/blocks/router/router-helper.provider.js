021d78dc0959e300baf21a057680227b75d9859b
Michael Ta <technical.ta@gmail.com>
2016-10-23 13:49:14 -0400

    revert change to HTML5 mode (renabled)

--- src/client/app/blocks/router/router-helper.provider.js
+++ src/client/app/blocks/router/router-helper.provider.js
@@ -19,7 +19,7 @@
       window.location.hash = '/';
     }
 
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
     this.configure = function(cfg) {
       angular.extend(config, cfg);
