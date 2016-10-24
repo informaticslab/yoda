4204d36e65e6ca58c943de13ca5ec3b784d5730e
Michael Ta <technical.ta@gmail.com>
2016-10-24 15:49:02 -0400

    testing path

--- src/server/app.js
+++ src/server/app.js
@@ -67,13 +67,13 @@ if(environment === 'build') {
 
   // case 'build':
     console.log('** BUILD **');
-    app.use(express.static('../../build/'));
+    app.use(express.static('./build/'));
     // Any invalid calls for templateUrls are under app/* and should return 404
     app.use('/app/*', function(req, res, next) {
       four0four.send404(req, res);
     });
     // Any deep link calls should return index.html
-    app.use('/*', express.static('../../build/index.html'));
+    app.use('/*', express.static('./build/index.html'));
 
 
     https.createServer(options, app).listen('4400');
